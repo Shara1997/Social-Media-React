b569905b94d775bcfc9b524ccbd3987a246dee67
Shara Raj <shararaj1997@gmail.com>
2023-12-20 15:54:54 +0530

    Updated sidebar to show different component on click

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Create Post");
+
   return (
     <>
       <div className="app-container">
-        <Sidebar />
+        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
         <div className="content-container">
           <Header />
-          <CreatePost />
-          <PostList />
+          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+
           <Footer />
         </div>
       </div>
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,37 +1,41 @@
 const CreatePost = () => {
   return (
     <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="htmlForm-label">
           Email address
         </label>
         <input
           type="email"
-          class="form-control"
+          className="htmlForm-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
         />
-        <div id="emailHelp" class="form-text">
+        <div id="emailHelp" className="htmlForm-text">
           We'll never share your email with anyone else.
         </div>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="htmlForm-label">
           Password
         </label>
         <input
           type="password"
-          class="form-control"
+          className="htmlForm-control"
           id="exampleInputPassword1"
         />
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">
+      <div className="mb-3 htmlForm-check">
+        <input
+          type="checkbox"
+          className="htmlForm-check-input"
+          id="exampleCheck1"
+        />
+        <label className="htmlForm-check-label" htmlFor="exampleCheck1">
           Check me out
         </label>
       </div>
-      <button type="submit" class="btn btn-primary">
+      <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
